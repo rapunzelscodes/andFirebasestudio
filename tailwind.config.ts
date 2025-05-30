635f129903b932cf4515d202837de0c4ed1d39fb
M T <emremer@gmail.com>
2025-05-30 14:14:06 +0000

    Update app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
   	extend: {
+      fontFamily: {
+        sans: ['var(--font-open-sans)', 'sans-serif'],
+      },
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -82,11 +85,16 @@ export default {
   				to: {
   					height: '0'
   				}
-  			}
+  			},
+        'ping-slow': {
+          '0%, 100%': { transform: 'scale(1)', opacity: '0.5' },
+          '50%': { transform: 'scale(1.1)', opacity: '0.2' },
+        }
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+        'ping-slow': 'ping-slow 3s cubic-bezier(0, 0, 0.2, 1) infinite',
   		}
   	}
   },
